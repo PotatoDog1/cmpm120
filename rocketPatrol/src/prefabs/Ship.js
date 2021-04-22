8cb3b2fcada3b620fb72a25cc119b1ac62a4200d
Gillian Yu <gwyu@ucsc.edu>
2021-04-21 20:48:59 -0700

    Readded back rocketPatrol directory, which was mistakenly deleted in the push of rocketPatrolMod.

---
+++ rocketPatrol/src/prefabs/Ship.js
@@ -0,0 +1,24 @@
+// Spaceship prefab
+class Ship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.movementSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update() {
+        // move spaceship left
+        this.x -= this.movementSpeed;
+        // wrap around from left edge to right edge
+        if (this.x <= 0 - this.width) {
+            // this.x = game.config.width;
+            this.reset();
+        }
+    }
+
+    // position reset
+    reset() {
+        this.x = game.config.width;  //+ 50
+    }
+}
